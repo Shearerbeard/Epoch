c666be00cb41b0529b4c4a65ed20efc145cf8f49
Mike Shearer <shearerbeard@gmail.com>
2022-12-21 15:55:31 -0700

    WIP: tests for in-memory decider example

--- src/decider.rs
+++ src/decider.rs
@@ -3,9 +3,9 @@ use std::marker::PhantomData;
 use async_trait::async_trait;
 
 trait Command {
-    type State: Default + Clone;
+    type State: Clone;
 }
-trait Event {}
+pub trait Event {}
 
 #[async_trait]
 trait Decider<S, Cmd: Command, E: Event, Err> {
@@ -50,11 +50,35 @@ impl<C: Command, E: Event + Clone> EventRepository<C, E, InMemoryEventRepository
     }
 }
 
+impl<C: Command, E: Event + Clone> InMemoryEventRepository<C, E> {
+    fn new() -> Self {
+        let events: Vec<E> = vec![];
+
+        Self {
+            events,
+            position: Default::default(),
+            pd: PhantomData::<C>::default(),
+        }
+    }
+}
+
+#[derive(Debug)]
 enum InMemoryEventRepositoryError {}
 
-#[derive(Default)]
 struct InMemoryStateRepository<C: Command> {
     state: <C as Command>::State,
+}
+
+impl<C> InMemoryStateRepository<C>
+where
+    C: Command,
+    <C as Command>::State: Default,
+{
+    fn new() -> Self {
+        Self {
+            state: <C as Command>::State::default(),
+        }
+    }
 }
 
 impl<C: Command> StateRepository<C, InMemoryEventRepositoryError> for InMemoryStateRepository<C> {
@@ -77,6 +101,10 @@ enum InMemoryStateRepositoryError {}
 mod tests {
     use std::collections::HashMap;
 
+    use thiserror::Error;
+
+    use self::user::UserFieldError;
+
     use super::*;
 
     trait ValueType<T> {
@@ -84,21 +112,27 @@ mod tests {
     }
 
     mod user {
+        use thiserror::Error;
+
         use super::ValueType;
 
         #[derive(Clone)]
         pub struct User {
             pub id: UserId,
-            pub name: String,
+            pub name: UserName,
         }
 
         pub type UserId = usize;
+
+        pub type UnvalidatedUserName = String;
+
+        #[derive(Clone)]
         pub struct UserName(String);
 
-        impl TryFrom<&str> for UserName {
+        impl TryFrom<String> for UserName {
             type Error = UserFieldError;
 
-            fn try_from(value: &str) -> Result<Self, Self::Error> {
+            fn try_from(value: String) -> Result<Self, Self::Error> {
                 let len = value.len();
                 if len < 1 {
                     Err(UserFieldError::EmptyName)
@@ -116,19 +150,22 @@ mod tests {
             }
         }
 
+        #[derive(Debug, Error)]
         pub enum UserFieldError {
+            #[error("Username cannot be empty")]
             EmptyName,
+            #[error("Username {0} is to long")]
             NameToLong(String),
         }
     }
 
-    #[derive(Default, Clone)]
+    #[derive(Clone, Default)]
     struct UserDeciderState {
         users: HashMap<user::UserId, user::User>,
     }
 
     enum UserCommand {
-        AddUser(user::UserName),
+        AddUser(user::UnvalidatedUserName),
         UpdateUserName(user::UserId, user::UserName),
     }
 
@@ -136,6 +173,7 @@ mod tests {
         type State = UserDeciderState;
     }
 
+    #[derive(Clone)]
     enum UserEvent {
         UserAdded(user::User),
         UserNameUpdated(user::UserId, user::UserName),
@@ -150,8 +188,16 @@ mod tests {
             cmd: UserCommand,
             state: UserDeciderState,
         ) -> Result<Vec<UserEvent>, UserDeciderError> {
-            match cmd  {
-                UserCommand::AddUser(_) => todo!(),
+            match cmd {
+                UserCommand::AddUser(user_name) => {
+                    let name = user::UserName::try_from(user_name)
+                        .map_err(|e| UserDeciderError::UserField(e))?;
+
+                    Ok(vec![UserEvent::UserAdded(user::User {
+                        id: 1,
+                        name
+                    })])
+                }
                 UserCommand::UpdateUserName(_, _) => todo!(),
             }
         }
@@ -168,8 +214,31 @@ mod tests {
         }
     }
 
-    enum UserDeciderError {}
+    #[derive(Debug, Error)]
+    enum UserDeciderError {
+        #[error("Invalid user field {0:?}")]
+        UserField(UserFieldError),
+    }
 
     #[test]
-    fn test_in_memory() {}
+    fn test_in_memory() {
+        let event_repository: InMemoryEventRepository<UserCommand, UserEvent> =
+            InMemoryEventRepository::new();
+        let state_repository: InMemoryStateRepository<UserCommand> = InMemoryStateRepository::new();
+
+        let state = event_repository
+            .load()
+            .expect("Empty Events Vector")
+            .into_iter()
+            .fold(UserDecider::init(), UserDecider::evolve);
+
+        let cmd = UserCommand::AddUser("Mike".to_string() as user::UnvalidatedUserName);
+        let events = UserDecider::decide(cmd, state).expect("Decider Success");
+
+        if let Some(UserEvent::UserAdded(user::User{ name, .. })) = events.first() {
+            assert_eq!(name.value(), "Mike".to_string())
+        } else {
+            panic!("Events not produced")
+        }
+    }
 }
